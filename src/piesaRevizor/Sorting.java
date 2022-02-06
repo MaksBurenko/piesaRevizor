8f1eeead558cba6b5e50b12e793fd6cfb3fda5d7
Maksim Burenko <Cherniymaks@yandex.ru>
2022-02-06 21:24:51 +0300

    123

--- src/piesaRevizor/Sorting.java
+++ src/piesaRevizor/Sorting.java
@@ -1,11 +1,12 @@
 package piesaRevizor;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Sorting {
     public static void main(String[] args) {
         String[] roles = {"Городничий", "Аммос Федорович", "Артемий Филиппович", "Лука Лукич"};
-        String[] textLines = {"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+        String[] textLines = {
+        "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
         "Аммос Федорович: Как ревизор?",
         "Артемий Филиппович: Как ревизор?",
         "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
@@ -13,8 +14,18 @@ public class Sorting {
         "Артемий Филиппович: Вот не было заботы, так подай!",
         "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
-        for (int i = 0; i< roles.length; i++) {
-            ArrayList al = new ArrayList(String roles[i]);
+        NavigableMap <String, List<String>> manRoles = new TreeMap();
+
+        for (String temp : roles) {
+            manRoles.put(temp, new ArrayList());
+        }
+
+        for (int i = 1;i < textLines.length;i++) {
+            for (String temp : manRoles.keySet()) {
+                if (textLines[i].startsWith(manRoles.get(temp))) {
+
+                }
+            }
         }
     }
 }
